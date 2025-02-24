ea41735b0acf9e19551b37c2766746db7839364a
rithwiksrivastav4 <srivastavrithwik@gmail.com>
2025-02-24 21:31:33 +0530

    added githud actions pipeline

--- aws/activity1/main.tf
+++ aws/activity1/main.tf
@@ -9,44 +9,49 @@ resource "aws_vpc" "base" {
 
 resource "aws_subnet" "one" {
   availability_zone = "ap-south-1a"
-  vpc_id            = "vpc-0ab3a0e99702294f6"
+  vpc_id            = aws_vpc.base.id
   cidr_block        = "10.0.0.0/24"
   tags = {
     Name    = "web"
     Env     = "dev"
     Project = "learning"
   }
+
+  depends_on = [ aws_vpc.base ]
 }
 
 resource "aws_subnet" "two" {
   availability_zone = "ap-south-1b"
-  vpc_id            = "vpc-0ab3a0e99702294f6"
+  vpc_id            = aws_vpc.base.id
   cidr_block        = "10.0.1.0/24"
   tags = {
     Name    = "db1"
     Env     = "dev"
     Project = "learning"
   }
+  depends_on = [ aws_vpc.base ]
 }
 
 resource "aws_subnet" "three" {
   availability_zone = "ap-south-1a"
-  vpc_id            = "vpc-0ab3a0e99702294f6"
+  vpc_id            = aws_vpc.base.id
   cidr_block        = "10.0.2.0/24"
   tags = {
     Name    = "db2"
     Env     = "dev"
     Project = "learning"
   }
+  depends_on = [ aws_vpc.base ]
 }
 
 resource "aws_subnet" "four" {
   availability_zone = "ap-south-1b"
-  vpc_id            = "vpc-0ab3a0e99702294f6"
+  vpc_id            = aws_vpc.base.id
   cidr_block        = "10.0.3.0/24"
   tags = {
     Name    = "app"
     Env     = "dev"
     Project = "learning"
   }
+  depends_on = [ aws_vpc.base ]
 }
